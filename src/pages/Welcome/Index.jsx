20b6b7b1b41f1299fd6cdcea7fdec9afe2c5e810
Manrriquez <manrriquez.contato@gmail.com>
2022-12-05 19:31:30 -0300

    implementando keyframes & finalizando section home da pagina welcome

--- src/pages/Welcome/Index.jsx
+++ src/pages/Welcome/Index.jsx
@@ -1,9 +1,12 @@
 import styled from '@emotion/styled';
-import { Avatar, Box, Container, Grid, Typography } from '@mui/material';
+import { Avatar, Box, Container, Grid, Paper, Typography } from '@mui/material';
 import React from 'react'
 import Navbar from '../../components/Navbar/Index';
 import ProgramingHome from '../../_assets/img/programing-home.svg';
-import JavaIcon from '../../_assets/img/java.svg'
+import JavaIcon from '../../_assets/img/java.svg';
+import AwsIcon from '../../_assets/img/aws.svg';
+import DockerIcon from '../../_assets/img/docker.svg';
+import ReactIcon from '../../_assets/img/reactjs.svg';
 
 
 const StyledTypograpyTitle = styled(Typography)(({ theme }) => ({
@@ -33,12 +36,90 @@ const StyledBoxBackground = styled(Box)(({ theme }) => ({
 
 const StyledAvatarAnimation = styled(Avatar)(({ theme }) => ({
     position: 'relative',
-    padding: '10px 15px 10px 15px',
+    padding: '10px',
     borderRadius: '8px',
-    width: '30px',
+    width: '45px',
+    height: '45px',
     background: '#FFFFFF',
+	  // animation: 'animationAvatar1 8s linear 0s infinite',
     border: '1px solid #D9D9D9',
     boxShadow: '0px 4px 40px rgb(9 9 12 / 10%)',
+    fontSize: '20px',
+
+    "@keyframes animationAvatar1": {
+      "0%": {
+        transform: "translateY(0)",
+      },
+      "10%": {
+	    	transform: "translateY(-8px)",
+      },
+      "30%": {
+	    	transform: "translateY(-8px)",
+      },
+      "50%": {
+	    	transform: "translateY(-8px)",
+      },
+      "70%": {
+	    	transform: "translateY(-8px)",
+      },
+      "20%": {
+	    	transform: "translateY(8px)",
+      },
+      "40%": {
+	    	transform: "translateY(8px)",
+      },
+      "60%": {
+	    	transform: "translateY(8px)",
+      },
+      "80%": {
+        transform: "translateY(6.4px)",
+      },
+      "90%": {
+		    transform: "translateY(-6.4px)",
+      },
+      "100%": {
+        transform: "translateY(0)",
+      }
+    },
+    "@keyframes animationAvatar2": {
+      "0%": {
+        transform: "translateY(0)",
+      },
+      "10%": {
+	    	transform: "translateY(8px)",
+      },
+      "30%": {
+	    	transform: "translateY(8px)",
+      },
+      "50%": {
+	    	transform: "translateY(8px)",
+      },
+      "70%": {
+	    	transform: "translateY(8px)",
+      },
+
+
+      "20%": {
+	    	transform: "translateY(-8px)",
+      },
+      "40%": {
+	    	transform: "translateY(-8px)",
+      },
+      "60%": {
+	    	transform: "translateY(-8px)",
+      },
+
+
+      "80%": {
+        transform: "translateY(-6.4px)",
+      },
+      "90%": {
+		    transform: "translateY(6.4px)",
+      },
+      "100%": {
+        transform: "translateY(0)",
+      }
+    },
 }));
 
 
@@ -48,7 +129,7 @@ const Welcome = () => {
       <Navbar />
       <Container sx={{marginTop: 5}} component="main" maxWidth="lg">
           <Box component="section">
-              <Grid container spacing={2} sx={{alignItems: 'center'}}>
+              <Grid container spacing={2} sx={{alignItems: 'center', mt: 6}}>
                   <Grid item  sm={12}  xs={12} md={7}>
                     <Box>
                         <StyledTypograpyTitle component="h1">
@@ -60,10 +141,14 @@ const Welcome = () => {
                     </Box>
                   </Grid>
 
-                  <Grid item sm={12} xs={12} md={5}>
+                  <Grid item sm={12} xs={12} md={5} sx={{mt: {xs: '5rem', md: 0} }}>
                     <Box sx={{width: '100%'}}>
-                        <StyledBoxBackground>
-                          <StyledAvatarAnimation src={JavaIcon}/>
+                        <StyledBoxBackground sx={{display: 'flex', justifyContent: 'space-around'}}>
+                          <StyledAvatarAnimation src={JavaIcon} sx={{top: '100px', animation: 'animationAvatar1 8s linear 0s infinite'}}/>
+                          <StyledAvatarAnimation src={DockerIcon} sx={{bottom: {md: '20px', xs: '40px'},
+                           animation: 'animationAvatar2 8s linear 0s infinite'}}/>
+                          <StyledAvatarAnimation src={AwsIcon} sx={{top: {md: '58px', xs: '20px'}, animation: 'animationAvatar1 8s linear 0s infinite'}}/>
+                          <StyledAvatarAnimation src={ReactIcon} sx={{animation: 'animationAvatar2 8s linear 0s infinite'}}/>
                         </StyledBoxBackground>
                     </Box>
                   </Grid>
