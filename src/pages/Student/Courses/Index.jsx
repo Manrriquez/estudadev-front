2e844838ba684ec114e210919754e24fc544790d
Manrriquez <manrriquez.contato@gmail.com>
2022-12-19 16:30:10 -0300

    adicionando cards & tabs na pagina de meus cursos

--- src/pages/Student/Courses/Index.jsx
+++ src/pages/Student/Courses/Index.jsx
@@ -5,8 +5,11 @@ import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
-import { Grid } from '@mui/material';
+import { Card, CardActionArea, CardContent, CardMedia, Grid, Link } from '@mui/material';
 import PhoneIcon from '@mui/icons-material/Phone';
+import IconButton from '@mui/material/IconButton';
+import DeleteIcon from '@mui/icons-material/Delete';
+import PlayArrowIcon from '@mui/icons-material/PlayArrow';
 
 
 
@@ -61,9 +64,10 @@ const Courses = () => {
             <Box sx={{ borderBottom: 0, borderColor: 'divider' }}>
                 <Tabs value={value} onChange={handleChange}  centered textColor="secondary"
   indicatorColor="secondary">
-                  <Tab icon={<PhoneIcon />}  label="Meus cursos" {...a11yProps(0)} />
-                  <Tab icon={<PhoneIcon />} label="Listas de desejos" {...a11yProps(1)} />
-                  <Tab icon={<PhoneIcon />} label="Minhas listas" {...a11yProps(2)} />
+                  <Tab icon={<PhoneIcon />}  label="MEUS CURSOS" {...a11yProps(0)} />
+                  <Tab icon={<PhoneIcon />} label="LISTA DE DESEJOS" {...a11yProps(1)} />
+                  <Tab icon={<PhoneIcon />} label="MINHAS LISTAS" {...a11yProps(2)} />
+                  <Tab icon={<PhoneIcon />} label="NOTICIAS" {...a11yProps(3)} />
                 </Tabs>
                 
             </Box>
@@ -72,19 +76,61 @@ const Courses = () => {
       </Grid>
 
       <Grid item md={12}>
-          <MainCard>
             <Box sx={{ width: '100%' }}>
                 <TabPanel value={value} index={0}>
-                  Item One
+                  <Box sx={{display: 'flex', justifyContent: 'space-around', gap: '10px'}}>
+                      <Box>
+                          <Card sx={{ maxWidth: 345 }} color="primary">
+                              <Link color="primary" component="a" href="#" sx={{textDecoration: 'none'}}>
+
+                                <CardMedia component="div"  
+                                  sx={{backgroundImage: 'url(https://mui.com/static/images/cards/contemplative-reptile.jpg)',
+                                  minHeight: '170px', p: 3, display: 'flex', justifyContent: 'center', alignItems: 'center',
+                                  '& .MuiIconButton-root': {
+                                      p: 2,
+                                    '&:hover': {
+                                      backgroundColor: 'orange!important',
+                                      display: 'block!important'
+                                    }
+                                  }, '&:hover': {display: 'block!important'} }}>
+                                   
+                                    <IconButton  size="large" 
+                                    sx={{backgroundColor: 'secondary.light', '&:hover': {backgroundColor: 'secondary.light'}, display: 'none' }}>
+                                      <PlayArrowIcon color='primary' fontSize="inherit" sx={{color: 'secondary.dark'}} />
+                                    </IconButton>
+                                 </CardMedia>
+
+                                <CardContent>
+                                  <Typography gutterBottom variant="h5" component="div">
+                                    Lizard
+                                  </Typography>
+                                  <Typography variant="body2" color="text.secondary">
+                                    Lizards are a widespread group of squamate reptiles, with over 6,000
+                                    species, ranging across all continents except Antarctica
+                                  </Typography>
+                                </CardContent>
+
+                              </Link>
+                          </Card>
+                      </Box>
+                  </Box>
                 </TabPanel>
+
+
                 <TabPanel value={value} index={1}>
                   Item Two
                 </TabPanel>
+
+
                 <TabPanel value={value} index={2}>
                   Item Three
                 </TabPanel>
+
+
+                 <TabPanel value={value} index={3}>
+                  Item Three 3333
+                </TabPanel>
             </Box>
-          </MainCard>
       </Grid>
     </Grid>
   )
